a6b64e503114793e38814723078fcae2553b0bbd
zakheledyosba <zakheledyosiba94@gmail.com>
2024-01-12 11:13:20 -0800

    more data structures)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    ret = list(a_dictionary.keys())[0]
+   
+   ret = list(a_dictionary.keys())[0]
     big = a_dictionary[ret]
 
     for k, v in a_dictionary.items():
