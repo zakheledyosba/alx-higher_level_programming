af9fa0913656a8331614ce1aedbd002b7aedb4d0
zakheledyosba <zakheledyosiba94@gmail.com>
2024-01-31 19:00:10 +0000

    updated

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,7 +4,8 @@
 
 class Rectangle:
     """Represent a rectangle."""
-     def __init__(self, width=0, height=0):
+
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
