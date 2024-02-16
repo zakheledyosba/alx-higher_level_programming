f1aeff4d0e25e6ec6ce7d4ae48b7430f992df90c
zakheledyosba <zakheledyosiba94@gmail.com>
2024-02-16 03:18:27 -0800

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a Base class."""
+
+
+class Base:
+    """Represents a base class for other classes."""
+
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """Initializes a Base instanace with an id.
+
+
+        Args:
+            id (int): The id of the instance.Automatically assigns a new id,
+            if None.
+
+        Attributes:
+            id (int): The id of the instance.
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects =+ 1
+            self.id = base.__nb_objects =+ 1
